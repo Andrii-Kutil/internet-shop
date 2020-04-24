5da40c6ccc58a5e2ab7d2ef031e54b1e047188e3
Superman696 <61060058+Superman696@users.noreply.github.com>
2020-04-24 08:38:34 +0300

    added impl service interfaces

---
+++ src/main/java/internetshop/service/impl/OrderServiceImpl.java
@@ -0,0 +1,42 @@
+package internetshop.service.impl;
+
+import internetshop.dao.OrderDao;
+import internetshop.lib.Inject;
+import internetshop.lib.Service;
+import internetshop.model.Order;
+import internetshop.model.Product;
+import internetshop.model.User;
+import internetshop.service.OrderService;
+import java.util.List;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+
+    @Inject
+    OrderDao orderDao;
+
+    @Override
+    public Order completeOrder(List<Product> products, User user) {
+        return orderDao.completeOrder(products, user);
+    }
+
+    @Override
+    public List<Order> getUserOrders(User user) {
+        return orderDao.getUserOrders(user);
+    }
+
+    @Override
+    public Order get(Long id) {
+        return orderDao.get(id).get();
+    }
+
+    @Override
+    public List<Order> getAll() {
+        return orderDao.getAll();
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return orderDao.delete(id);
+    }
+}
