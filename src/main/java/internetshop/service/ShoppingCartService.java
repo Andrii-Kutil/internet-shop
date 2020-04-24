d3faaee6b253be4cfc3d874dcbc578f07d7d93ad
Superman696 <61060058+Superman696@users.noreply.github.com>
2020-04-24 08:38:56 +0300

    added service interfaces

---
+++ src/main/java/internetshop/service/ShoppingCartService.java
@@ -0,0 +1,19 @@
+package internetshop.service;
+
+import internetshop.model.Product;
+import internetshop.model.ShoppingCart;
+import java.util.List;
+
+public interface ShoppingCartService {
+    ShoppingCart create(ShoppingCart shoppingCart);
+
+    ShoppingCart addProduct(ShoppingCart shoppingCart, Product product);
+
+    boolean deleteProduct(ShoppingCart shoppingCart, Product product);
+
+    void clear(ShoppingCart shoppingCart); //remove all products from the shoppingCart
+
+    ShoppingCart getByUserId(Long userId);
+
+    List<Product> getAllProducts(ShoppingCart shoppingCart);
+}
