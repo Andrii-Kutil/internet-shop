babe5bf4d8e8b98ff7562d8d825a56fac06ea593
Superman696 <61060058+Superman696@users.noreply.github.com>
2020-04-27 00:21:13 +0300

    added generic service

--- src/main/java/internetshop/service/impl/OrderServiceImpl.java
+++ src/main/java/internetshop/service/impl/OrderServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class OrderServiceImpl implements OrderService {
 
     @Inject
-    OrderDao orderDao;
+    private OrderDao orderDao;
     @Inject
     private ShoppingCartService cartService;
 
