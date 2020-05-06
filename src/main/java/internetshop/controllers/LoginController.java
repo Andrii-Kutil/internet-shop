885174a1851fc316d51ba670933650364c4bc71a
Andrii <61060058+Andrii-Kutil@users.noreply.github.com>
2020-05-06 18:48:42 +0300

    added logger (#11)
    * added logger
    
    * changed type of logger
    
    * fixed exception's information
    
    * used two parameters instead one
    
    Co-authored-by: Superman696 <61060058+Superman696@users.noreply.github.com>

--- src/main/java/internetshop/controllers/LoginController.java
+++ src/main/java/internetshop/controllers/LoginController.java
@@ -10,11 +10,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.apache.log4j.Logger;
 
 public class LoginController extends HttpServlet {
     private static Injector injector = Injector.getInstance("internetshop");
     private final AuthenticationService authService =
             (AuthenticationService) injector.getInstance(AuthenticationService.class);
+    private final Logger logger = Logger.getLogger(LoginController.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -32,6 +34,7 @@ public class LoginController extends HttpServlet {
             HttpSession session = req.getSession();
             session.setAttribute("userId", user.getId());
         } catch (AuthenticationException e) {
+            logger.warn(e);
             req.setAttribute("errorMsg", e.getMessage());
             req.getRequestDispatcher("WEB-INF/views/login.jsp").forward(req, resp);
         }
