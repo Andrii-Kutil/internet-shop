bb179a15c1351a25853a12f7a89d81bac6dbc153
Superman696 <61060058+Superman696@users.noreply.github.com>
2020-05-10 18:58:29 +0300

    added ProductDaoJdbcImpl

---
+++ src/main/java/internetshop/dao/jdbc/ProductDaoJdbcImpl.java
@@ -0,0 +1,111 @@
+package internetshop.dao.jdbc;
+
+import internetshop.dao.ProductDao;
+import internetshop.lib.Dao;
+import internetshop.model.Product;
+import internetshop.util.ConnectionUtil;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Dao
+public class ProductDaoJdbcImpl implements ProductDao {
+    private final Connection connection = ConnectionUtil.getConnection();
+
+    @Override
+    public Product create(Product product) {
+        String query = "INSERT INTO products (`name`, `price`) VALUES (?, ?)";
+        try (Connection connection = ConnectionUtil.getConnection()) {
+            PreparedStatement statement = connection
+                    .prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, product.getName());
+            statement.setBigDecimal(2, product.getPrice());
+            statement.executeUpdate();
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    product.setId(generatedKeys.getLong(1));
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Created is failed", e);
+        }
+        return product;
+    }
+
+    @Override
+    public Optional<Product> get(Long id) {
+        String query = "SELECT * FROM products WHERE id = ?";
+        try (Connection connection = ConnectionUtil.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setLong(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return Optional.of(getProductFromResultSet(resultSet));
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException("Product not found", ex);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Product> getAll() {
+        List<Product> products = new ArrayList<>();
+        String query = "SELECT * FROM products";
+        try (Connection connection = ConnectionUtil.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                products.add(getProductFromResultSet(resultSet));
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException("Products not found", ex);
+        }
+        return products;
+    }
+
+    @Override
+    public Product update(Product product) {
+        String query = "UPDATE products SET name = ?, price = ? WHERE id = ?";
+        try (Connection connection = ConnectionUtil.getConnection()) {
+            connection.setAutoCommit(false);
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, product.getName());
+            statement.setBigDecimal(2, product.getPrice());
+            statement.setLong(3, product.getId());
+            statement.executeUpdate();
+            connection.commit();
+        } catch (SQLException e) {
+            throw new RuntimeException("Product was'nt updated", e);
+        }
+        return product;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        String query = "DELETE FROM products WHERE id = ?";
+        try (Connection connection = ConnectionUtil.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setLong(1, id);
+            if (statement.executeUpdate() > 0) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+        return false;
+    }
+
+    public Product getProductFromResultSet(ResultSet rs) throws SQLException {
+        Long id = rs.getLong("id");
+        String name = rs.getString("name");
+        BigDecimal price = new BigDecimal(rs.getString("price"));
+        return new Product(id, name, price);
+    }
+}
